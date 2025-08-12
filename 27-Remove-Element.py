b15acea9e53d13d1a21a6ffebfa794b843148003
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2025-08-12 19:40:26 +0300

    Add simpler solution to #27

--- 27-Remove-Element.py
+++ 27-Remove-Element.py
@@ -18,3 +18,14 @@ class Solution:
             l += 1
 
         return k
+
+# Simpler Solution
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        
+        return k
