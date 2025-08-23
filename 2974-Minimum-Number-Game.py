16b35052ad0f8df00a454eb5998c22803f89581a
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2025-08-23 19:06:24 +0300

    Add 2nd solution for #2974

--- 2974-Minimum-Number-Game.py
+++ 2974-Minimum-Number-Game.py
@@ -10,3 +10,18 @@ class Solution:
             arr.append(nums[i + 1])
             arr.append(nums[i])
         return arr
+    
+# MinHeap solution
+# Time Complexity: O(n log n)
+# Space Complexity: O(n)
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        heapq.heapify(nums) 
+        arr = []
+
+        while nums:
+            first, second = heapq.heappop(nums), heapq.heappop(nums)
+            arr.append(second)
+            arr.append(first)
+
+        return arr
