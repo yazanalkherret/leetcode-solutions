62adbe85b88f2e5804c3b87832813ed3102f4aa5
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2025-09-07 18:38:07 +0300

    Add 2nd solution for #300

--- 300-Longest-Increasing-Subsequence.py
+++ 300-Longest-Increasing-Subsequence.py
@@ -11,3 +11,22 @@ class Solution:
                 patsort[bisect.bisect_left(patsort, i)] = i
             
         return len(patsort)
+
+# Dynamic Programming
+# Time Complexity: O(n^2)
+# Space Complexity: O(n)
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        N = len(nums)
+        memo = [1] * N
+        res = 1
+
+        for i in range(N - 1, -1, -1):
+            for j in range(i, N):
+                if nums[i] < nums[j]:
+                    memo[i] = max(memo[i], 1 + memo[j])
+            res = max(res, memo[i])
+
+        return res
+
