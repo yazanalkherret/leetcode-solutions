90d51b796c1d78115f87377b16fdc8e323cea36c
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2025-09-08 18:26:25 +0300

    Add solution for #1317

---
+++ 1317-Convert-Integer-to-the-Sum-of-Two-No-Zero-Integers.py
@@ -0,0 +1,9 @@
+# Time Complexity: O(n logn)
+# Space Complexity: O(1)
+
+class Solution:
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        for A in range(1, n):
+            B = n - A
+            if "0" not in str(A) + str(B):
+                return [A, B]
