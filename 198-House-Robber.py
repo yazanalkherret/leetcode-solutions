0578fc91e19dea892db8f87a558abd36c15cf5a8
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2025-09-13 12:01:28 +0300

    Add more solutions for #198

--- 198-House-Robber.py
+++ 198-House-Robber.py
@@ -1,3 +1,38 @@
+# Top-down Dynamic Programming
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = [-1] * len(nums)
+        def helper(i):
+            if i >= len(nums): return 0
+            if memo[i] != -1: return memo[i]
+            
+            memo[i] = max(nums[i] + helper(i + 2), helper(i + 1))
+            return memo[i]
+
+        helper(0)
+        return memo[0]
+
+# Bottom-up Dynamic Programming
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = nums[::]
+        memo.append(0)
+        memo.append(0)
+
+        for i in range(len(nums) - 1, -1, -1):
+            memo[i] = max(memo[i] + memo[i + 2], memo[i + 1])
+        return memo[0]
+
+# Bottom-up Dynamic Programming
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
         prev1, prev2 = 0, 0
