d098e209a1d197c35ae8954bd31803dc2412ae9f
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2025-09-28 19:12:55 +0300

    Add solution for #349

---
+++ 349-Intersection-of-Two-Arrays.py
@@ -0,0 +1,6 @@
+# Time Complexity: O(m+n)
+# Space Complexity: O(m+n), O(1)
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1) & set(nums2))
