6b0aeae800c7173f2b35ed783ba310b496855f30
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2025-09-30 11:55:24 +0300

    Add solution for #3692

---
+++ 3692-Majority-Frequency-Characters.py
@@ -0,0 +1,18 @@
+class Solution:
+    def majorityFrequencyGroup(self, s: str) -> str:
+        charFreq = Counter(s)
+        # char - > number
+        freqChars = defaultdict(list)
+        for char, freq in charFreq.items():
+            freqChars[freq].append(char)
+        print(freqChars)
+        # number -> char
+        maxSize = 0
+        res = None # Edge case 
+        for key, val in sorted(freqChars.keys()):
+            if len(freqChars[key]) >= maxSize:
+                maxSize = len(freqChars[key])
+                res = freqChars[key]
+
+        # Convert to string
+        return "".join(res)
