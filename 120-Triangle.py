549b75cfd65d20acd5b6487e4f6b50d35d0612fc
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2025-10-03 12:23:05 +0300

    Add 2nd solution for #120

--- 120-Triangle.py
+++ 120-Triangle.py
@@ -11,3 +11,17 @@ class Solution:
                 dp[i][j] = dp[i][j] + min(dp[i - 1][j], dp[i - 1][j - 1])
 
         return min(dp[-1])
+
+
+# Cleaner Solution
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        dp = [level[::] for level in triangle]
+        height = len(dp)
+        
+        for i in range(height - 2, -1, -1):
+            for j in range(len(dp[i])):
+                dp[i][j] += min(dp[i + 1][j], dp[i + 1][j + 1])
+
+        return dp[0][0]
