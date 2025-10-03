461c5da3ab150d9733c4b0a6bec90833f852c809
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2025-10-03 12:17:36 +0300

    Add solution for #3658

---
+++ 3658-GCD-of-Odd-and-Even-Sums.py
@@ -0,0 +1,8 @@
+# Time Complexity: O(1)
+# Space Complexity: O(1)
+
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        return n
+        # Sum of first n even numbers: n(n+1)
+        # Sum of first n odd numbers: n^2
