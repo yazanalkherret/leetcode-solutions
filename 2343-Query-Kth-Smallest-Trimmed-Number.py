9bef80842fdd2077e84cad1d3727b186dffe9dd1
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2025-10-19 02:39:07 +0300

    Add 2nd Solution for #2343

--- 2343-Query-Kth-Smallest-Trimmed-Number.py
+++ 2343-Query-Kth-Smallest-Trimmed-Number.py
@@ -13,3 +13,25 @@ class Solution:
 
             ans.append(heappop(minheap)[1])
         return ans
+
+# Time Complexity: O(maxTrim * n log n)
+# Space Complexity: O(maxTrim * n)
+
+class Solution:
+    def smallestTrimmedNumbers(self, nums: List[str], queries: List[List[int]]) -> List[int]:
+        maxTrim = max([t for _, t in queries])
+
+        trimmedSorted = []
+
+        # Construct table
+        for i in range(1, maxTrim + 1):
+            trimmed = []
+            for j, num in enumerate(nums):
+                trimmed.append((num[-i:], j))
+            trimmedSorted.append(sorted(trimmed))
+            
+        ans = []
+        for k, trim in queries:
+            ans.append(trimmedSorted[trim - 1][k - 1][1])
+
+        return ans
