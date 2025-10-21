e0a52f247e5b66a85a58ca0858d6d6add7326f40
Yazan Alkherret <186625651+yazanalkherret@users.noreply.github.com>
2025-10-21 16:23:09 +0300

    Add solution for #2011

---
+++ 2011-Final-Value-of-Variable-After-Performing-Operaitons.py
@@ -0,0 +1,12 @@
+# Time Complexity: O(n), n -> operations
+# Space Complexity: O(1)
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        x = 0
+        for op in operations:
+            if "--" in op:
+                x -= 1
+            else:
+                x += 1
+        return x
