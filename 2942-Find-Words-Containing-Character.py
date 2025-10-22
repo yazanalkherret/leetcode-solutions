bbf715d6aba1d7d61cf5efda6cc91eb336153fbf
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2025-10-22 16:35:53 +0300

    Add solution for #2942

---
+++ 2942-Find-Words-Containing-Character.py
@@ -0,0 +1,10 @@
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        ans = []
+        for i, w in enumerate(words):
+            if x in w:
+                ans.append(i)
+        return ans
