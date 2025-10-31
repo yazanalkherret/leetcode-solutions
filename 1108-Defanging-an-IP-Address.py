9f0721d9d64b4e247d9fa22e16973c397f02ef62
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2025-10-31 12:03:35 +0300

    Add solution for #1108

---
+++ 1108-Defanging-an-IP-Address.py
@@ -0,0 +1,12 @@
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        ans = []
+        for char in address:
+            if char == ".":
+                ans.append("[.]")
+            else:
+                ans.append(char)
+        return "".join(ans)
