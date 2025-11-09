5a34ccfdacea77fa4547e70cd2d894a83ac498ea
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2025-11-09 21:14:43 +0300

    Add solution #3740

---
+++ 3740-Minimum-Distance-Between-Three-Equal-Elements-I.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+        indices = defaultdict(list)
+
+        for i, num in enumerate(nums):
+            indices[num].append(i)
+
+        ans = float("inf")
+        for num, vals in indices.items():
+            if len(vals) >= 3:
+                for i in range(2, len(vals)):
+                    ans = min(ans,
+                             abs(vals[i] - vals[i - 1]) +
+                             abs(vals[i - 1] - vals[i - 2]) +
+                             abs(vals[i] - vals[i - 2])
+                             )
+
+
+        return -1 if ans == float("inf") else ans
+        
