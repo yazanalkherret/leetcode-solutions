1024afac8efdeb073171e05e716429f92d2ed466
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2025-11-10 15:37:58 +0300

    Add solution #168

---
+++ 168-Excel-Sheet-Column-Title.py
@@ -0,0 +1,13 @@
+# Time Complexity: O(log n)
+# Space Complexity: O(log n)
+
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        ans = ""
+        while columnNumber > 0:
+            columnNumber -= 1
+            ans += chr(columnNumber % 26 + ord("A"))
+            columnNumber //= 26
+
+        
+        return ans[::-1]
