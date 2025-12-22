f986881172b16b6ace6e9d8e9f045040a9a72145
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2025-12-22 06:13:53 +0300

    Add other solution #1425

--- 1425-Constrained-Subsequence-Sum.py
+++ 1425-Constrained-Subsequence-Sum.py
@@ -17,3 +17,26 @@ class Solution:
             heappush(heap, (-curr, j))
 
         return best_res
+
+# TLE
+
+class Solution:
+    def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+
+        @lru_cache(None)
+        def dp(j):
+            if j == 0: return nums[0]
+
+            best_sum = nums[j]
+            for i in range(max(0, j - k), j):
+                best_sum = max(best_sum, nums[j] + dp(i))
+
+            return best_sum
+
+        best_res = nums[0]
+        for j in range(n - 1, -1, -1):
+            best_res = max(best_res, dp(j))
+
+        return best_res
+        
