7d542d0096da35d48f31450e4d8b960128430de9
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2025-12-25 07:25:45 +0300

    Add 2nd solution #1020

--- 1020-Number-of-Enclaves.py
+++ 1020-Number-of-Enclaves.py
@@ -1,4 +1,35 @@
-# Solution #1
+# DFS
+# Time Complexity: O(m * n)
+# Space Complexity: O(m * n)
+
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        directions = [(1, 0), (0, 1), (0, -1), (-1, 0)]
+        copy = [row[::] for row in grid]
+
+        def dfs(row, col):
+            copy[row][col] = 0
+
+            for dr, dc in directions:
+                nei_row, nei_col = row + dr, col + dc
+                if 0 <= nei_row < m and 0 <= nei_col < n and copy[nei_row][nei_col]:
+                    dfs(nei_row, nei_col)
+
+        for i in range(m):
+            for j in range(n):
+                if copy[i][j] and (i == 0 or i == m - 1 or j == 0 or j == n - 1):
+                    dfs(i, j)
+
+        enclaves = 0
+        for i in range(m):
+            for j in range(n):
+                if copy[i][j]:
+                    enclaves += 1
+        return enclaves
+
+# Solution #2
+
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
