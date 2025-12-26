f6796374be5e7d2fceb5020cc71cfe3c9460fa52
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2025-12-26 17:21:51 +0300

    Add solution #49

---
+++ 49-Group-Anagrams.py
@@ -0,0 +1,9 @@
+# Time Complexity: O(N(K logK))
+# Space Complexity: O(N * K)
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+        for s in strs:
+            ans[tuple(sorted(s))].append(s)
+        return list(ans.values())
