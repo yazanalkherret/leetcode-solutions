0c7faf25209877b042eaa5bcd951bbb778158496
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2025-12-31 06:31:42 +0300

    Add solution #167

---
+++ python/167-Two-Sum-II-Input-Array-Is-Sorted.py
@@ -0,0 +1,17 @@
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            cur_sum = numbers[l] + numbers[r]
+            if cur_sum == target:
+                return [l + 1, r + 1]
+            elif cur_sum > target:
+                r -= 1
+            else:
+                l += 1
+
+        return [-1, -1]
