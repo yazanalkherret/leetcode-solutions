f6ca0f441046534bdcd33e7c115abe93d2aa5257
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2026-01-01 16:54:59 +0300

    Add solution #235

---
+++ python/235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
@@ -0,0 +1,11 @@
+# Time Complexity: O(n)
+# Space Complexity: O(h)
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return root
