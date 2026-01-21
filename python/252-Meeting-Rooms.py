4f63ff921aa2db4537fe4edae331ce1f648c5a29
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2026-01-21 18:49:35 +0300

    Add solution #252

---
+++ python/252-Meeting-Rooms.py
@@ -0,0 +1,12 @@
+# Time Complexity: O(nlogn)
+# Space Complexity: O(n)
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        intervals = sorted(intervals)
+
+        for i in range(1, len(intervals)):
+            if intervals[i][0] < intervals[i - 1][1]:
+                return False
+
+        return True
