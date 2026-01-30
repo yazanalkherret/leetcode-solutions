99d0d2dedce04abc692ca6e008af350270c3fe68
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2026-01-30 12:57:31 +0300

    Add 2nd solution #23

--- python/23-Merge-k-Sorted-Lists.py
+++ python/23-Merge-k-Sorted-Lists.py
@@ -1,4 +1,36 @@
-# Can be solved many ways
+# Time Complexity: O(nlogk)
+# Space Complexity: O(k)
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        head = ListNode()
+
+        # Node Mapping
+        val_node_map = defaultdict(list)
+        heap = []
+        for node in lists:
+            if node:
+                heappush(heap, node.val)
+                val_node_map[node.val].append(node)
+
+        
+        res_ptr = head
+
+        while heap:
+            val = heappop(heap)
+            
+            node = val_node_map[val].pop()
+
+            # Add to res
+            res_ptr.next = ListNode(val)
+            res_ptr = res_ptr.next
+
+            if node.next:
+                heappush(heap, node.next.val)
+                val_node_map[node.next.val].append(node.next)
+
+        return head.next
 
 # Time Complexity: O(n log n) -> Sorting
 # Space Complexity: O(n) -> array/sorting
